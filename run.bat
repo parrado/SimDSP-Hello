a8b038ff1571dcd973cb3c2d90d26e5b64683f9f
Alexander López-Parrado <parrado@uniquindio.edu.co>
2022-06-06 22:50:10 +0200

    Fix batch file

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 call ndk-build clean
 call ndk-build
-call xcopy /Y obj\local\armeabi-v7a\simdsp simdspf\simdsp
+call xcopy /Y obj\local\armeabi-v7a\simdsp simdspf\simdsp*
 call adb push simdspf/. /data/local/tmp/simdspf
 
 
